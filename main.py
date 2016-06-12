e03e7486169a0baf8562ede866db7ed38cdb312c
Brady Auen <bradyauen@Bradys-MacBook-Pro.local>
2016-06-12 13:59:23 -0600

    done

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import tornado.web
 from tornado.options import define, options
 from tornado.httpserver import HTTPServer
 from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+import facebook
 import json
 import simplejson as json
 
@@ -15,10 +16,10 @@ class MainHandler(tornado.web.RequestHandler):
 
 class TwitterStreamHandler(tornado.web.RequestHandler):
 	def get(self):
-		ACCESS_TOKEN = "874249651-S1SofEKsa3CFzhrxy0tbEj3DxxXuNVBPYR03oxyj"
-		ACCESS_SECRET = "S53SzgFJ6R9k9awgaMErTn90dyUUrOfzdm3fqUxFEnzVq"
-		CONSUMER_KEY = 	"Zb18h6djL2I61LSrJGQDR4PbD"
-		CONSUMER_SECRET = "5xNWaF4sOhY9rrBkOdBIiREDUJXoyC9rcQY49US0U61mlutVlS"
+		ACCESS_TOKEN = "Not"
+		ACCESS_SECRET = "Something"
+		CONSUMER_KEY = 	"Im"
+		CONSUMER_SECRET = "Sharing"
 
 		oauth = OAuth(ACCESS_TOKEN,ACCESS_SECRET,CONSUMER_KEY,CONSUMER_SECRET)
 
@@ -39,31 +40,36 @@ class TwitterStreamHandler(tornado.web.RequestHandler):
 
 class TwitterRESTHandler(tornado.web.RequestHandler):
 	def get(self):
-		ACCESS_TOKEN = "874249651-S1SofEKsa3CFzhrxy0tbEj3DxxXuNVBPYR03oxyj"
-		ACCESS_SECRET = "S53SzgFJ6R9k9awgaMErTn90dyUUrOfzdm3fqUxFEnzVq"
-		CONSUMER_KEY = 	"Zb18h6djL2I61LSrJGQDR4PbD"
-		CONSUMER_SECRET = "5xNWaF4sOhY9rrBkOdBIiREDUJXoyC9rcQY49US0U61mlutVlS"
+		ACCESS_TOKEN = "Not"
+		ACCESS_SECRET = "Something"
+		CONSUMER_KEY = 	"Im"
+		CONSUMER_SECRET = "Sharing"
 
 		oauth = OAuth(ACCESS_TOKEN,ACCESS_SECRET,CONSUMER_KEY,CONSUMER_SECRET)
 
 		twitter = Twitter(auth = oauth)
 		iterator = twitter.search.tweets(q="#Broncos", result_type="recent", lang="en", count = 10)
 
-		print(json.dumps(iterator, indent=4))
-			
-
 		self.render('twitterstream.html', twitterJSON = json.dumps(iterator))
 
 class TwitterPOSTHandler(tornado.web.RequestHandler):
 	def post(self):
-		ACCESS_TOKEN = "874249651-S1SofEKsa3CFzhrxy0tbEj3DxxXuNVBPYR03oxyj"
-		ACCESS_SECRET = "S53SzgFJ6R9k9awgaMErTn90dyUUrOfzdm3fqUxFEnzVq"
-		CONSUMER_KEY = 	"Zb18h6djL2I61LSrJGQDR4PbD"
-		CONSUMER_SECRET = "5xNWaF4sOhY9rrBkOdBIiREDUJXoyC9rcQY49US0U61mlutVlS"
-
+		data = json.loads(self.request.body.decode('utf-8'))
+		ACCESS_TOKEN = "Not"
+		ACCESS_SECRET = "Something"
+		CONSUMER_KEY = 	"Im"
+		CONSUMER_SECRET = "Sharing"
 		oauth = OAuth(ACCESS_TOKEN,ACCESS_SECRET,CONSUMER_KEY,CONSUMER_SECRET)
 		twitter = Twitter(auth = oauth)
-		iterator = twitter.statuses.update(status = "Tweeting through Tornado, because this is what I do when I'm bored now, apparently.")
+		twitter.statuses.update(status = data)
+
+class FacebookPostHandler(tornado.web.RequestHandler):
+	def post(self):
+		data = json.loads(self.request.body.decode('utf-8'))
+		print(data)
+		oauth = "Not for you, GitHub user"
+		graph = facebook.GraphAPI(access_token = oauth, version ='2.2')
+		graph.put_object(parent_object = 'me', connection_name='feed', message = data)
 
 
 if __name__ == "__main__":
@@ -75,7 +81,8 @@ if __name__ == "__main__":
 		(r"/", MainHandler),
 		(r"/twitterstream", TwitterStreamHandler),
 		(r"/twitterREST", TwitterRESTHandler),
-		(r"/twitterPOST", TwitterPOSTHandler)
+		(r"/twitterPOST", TwitterPOSTHandler),
+		(r"/facebookPOST", FacebookPostHandler)
 	]
 	application = tornado.web.Application(handlers=routes, **settings)
 	server = HTTPServer(application, xheaders=True)
